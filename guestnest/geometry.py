9f5f8e90a6bb4e70f9effa8c282496d3472191cc
Conor Rankine <206134505+conorrankine@users.noreply.github.com>
2025-04-21 16:34:33 +0100

    moved geometric functions from `optimise.py` to `geometry.py`

---
+++ guestnest/geometry.py
@@ -0,0 +1,187 @@
+"""
+GUESTNEST
+Copyright (C) 2025  Conor D. Rankine
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either Version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+############################### LIBRARY IMPORTS ###############################
+###############################################################################
+
+import numpy as np
+from rdkit import Chem
+
+###############################################################################
+################################## FUNCTIONS ##################################
+###############################################################################
+
+def centre(
+    mol: Chem.Mol,
+    conf_idx: int = -1
+) -> None:
+    
+    _set_centre_of_mass(
+        mol, [0.0, 0.0, 0.0], conf_idx = conf_idx
+    )
+
+def translate_mol(
+    mol: Chem.Mol,
+    distances: np.ndarray,
+    conf_idx: int = -1
+) -> None:
+    
+    translated_coords = translate_coords(
+        get_coords(mol, conf_idx = conf_idx), distances
+    )
+    set_coords(mol, translated_coords, conf_idx = conf_idx)
+
+def rotate_mol(
+    mol: Chem.Mol,
+    angles: np.ndarray,
+    conf_idx: int = -1
+) -> None:
+    
+    rotated_coords = rotate_coords(
+        get_coords(mol, conf_idx = conf_idx), angles
+    )
+    set_coords(mol, rotated_coords, conf_idx = conf_idx)
+
+def translate_coords(
+    coords: np.ndarray,
+    distances: np.ndarray
+) -> np.ndarray:
+    
+    return coords + distances
+
+def rotate_coords(
+    coords: np.ndarray,
+    angles: np.ndarray,
+) -> np.ndarray:
+    
+    return coords @ _angles_to_rotation_matrix(angles).T
+
+def get_coords(
+    mol: Chem.Mol,
+    conf_idx: int = -1
+) -> np.ndarray:
+    
+    conf = mol.GetConformer(conf_idx)
+    return np.array(
+        [list(conf.GetAtomPosition(i)) for i in range(mol.GetNumAtoms())]
+    )
+
+def set_coords(
+    mol: Chem.Mol,
+    coords: np.ndarray,
+    conf_idx: int = -1
+) -> None:
+    
+    conf = mol.GetConformer(conf_idx)
+    for i, (x, y, z) in enumerate(coords):
+        conf.SetAtomPosition(i, (x, y, z))
+
+def _get_centre_of_mass(
+    mol: Chem.Mol,
+    conf_idx: int = -1
+) -> np.ndarray:
+    
+    masses = np.array([atom.GetMass() for atom in mol.GetAtoms()])
+    coords = get_coords(mol, conf_idx = conf_idx)
+    centre_of_mass = (
+        np.sum(coords * masses[:, None], axis = 0) / np.sum(masses)
+    )
+
+    return centre_of_mass
+
+def _set_centre_of_mass(
+    mol: Chem.Mol,
+    com: np.ndarray,
+    conf_idx: int = -1,
+) -> None:
+    
+    coords_init = get_coords(mol, conf_idx = conf_idx)
+    com_init = _get_centre_of_mass(mol, conf_idx = conf_idx)
+
+    set_coords(
+        mol, translate_coords(coords_init, com - com_init), conf_idx = conf_idx
+    )
+
+def _angles_to_rotation_matrix(
+    angles = np.ndarray
+) -> np.ndarray:
+    
+    alpha, beta, gamma = angles
+
+    rx = np.array([
+        [1.0,            0.0,            0.0          ],
+        [0.0,            np.cos(alpha), -np.sin(alpha)],
+        [0.0,            np.sin(alpha),  np.cos(alpha)]
+    ])
+    ry = np.array([
+        [ np.cos(beta),  0.0,             np.sin(beta)],
+        [ 0.0,           1.0,             0.0         ],
+        [-np.sin(beta),  0.0,             np.cos(beta)]
+    ])
+    rz = np.array([
+        [np.cos(gamma), -np.sin(gamma),   0.0         ],
+        [np.sin(gamma),  np.cos(gamma),   0.0         ],
+        [0.0,            0.0,             1.0         ]
+    ])
+
+    return rz @ ry @ rx
+
+def spherical_to_cartesian(
+    r: float,
+    theta: float,
+    phi: float
+) -> np.ndarray:
+    
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+
+    return np.array([x, y, z])
+
+def cartesian_to_spherical(
+    x: float,
+    y: float,
+    z: float
+) -> np.ndarray:
+
+    r = np.sqrt((x**2) + (y**2) + (z**2))
+    if r == 0.0:
+        return np.array([0.0, 0.0, 0.0])
+    else:
+        theta = np.arccos(z / r)
+        phi = np.arctan2(y, x)
+        return np.array([r, theta, phi])
+
+def get_vdw_distance_matrix(
+    host: Chem.Mol,
+    guest: Chem.Mol,
+    vdw_scaling: float = 1.0
+) -> np.ndarray:
+    
+    pt = Chem.GetPeriodicTable() 
+    
+    host_vdw = np.array(
+        [pt.GetRvdw(atom.GetSymbol()) for atom in host.GetAtoms()]
+    ) * vdw_scaling
+    guest_vdw = np.array(
+        [pt.GetRvdw(atom.GetSymbol()) for atom in guest.GetAtoms()]
+    ) * vdw_scaling
+    
+    vdw_distance_matrix = guest_vdw + host_vdw[:, None]
+
+    return vdw_distance_matrix
