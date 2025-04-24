47bcd418ab41499ab67a9065d73d29a27d0c72e6
Conor Rankine <206134505+conorrankine@users.noreply.github.com>
2025-04-24 12:16:12 +0100

    added docstring to the `_get_linkage_matrix()` function in `cluster.py`

--- guestnest/cluster.py
+++ guestnest/cluster.py
@@ -91,12 +91,31 @@ def _get_linkage_matrix(
     method: str = 'average',
     metric: str = 'euclidean'
 ) -> np.ndarray:
+    """
+    Calculates the hierarchical clustering linkage matrix from a square
+    symmetric distance matrix.
+
+    Args:
+        matrix (np.ndarray): Distance matrix as an array of shape (n, n).
+        method (str, optional): Linkage method; see the documentation for
+            scipy.cluster.hierarchy.linkage() for a full list of options.
+            Defaults to 'average'.
+        metric (str, optional): Linkage metric; see the documentation for
+            scipy.cluster.hierarchy.linkage() for a full list of options.
+            Defaults to 'euclidean'.
+
+    Raises:
+        ValueError: If `matrix` is not a square matrix.
+
+    Returns:
+        np.ndarray: Hierarchical clustering linkage matrix.
+    """
     
     dim1, dim2 = matrix.shape
     if dim1 != dim2:
         raise ValueError(
-            '`matrix` should be an array of shape (N,N), i.e. a 2D array with '
-            f'equal dimensions; got an array of shape {matrix.shape}'
+            '`matrix` should be an array of shape (n, n), i.e. a 2D array '
+            f'with equal dimensions; got an array of shape {matrix.shape}'
         )
 
     return linkage(
